202dfb58d01643f9c7e20a29db0ec39a238e5ff6
Cristian Fabian Martinez <cris.mtxz@gmail.com>
2021-04-23 22:06:21 +0200

    red, new test none intersection

--- src/test/java/usantatecla/IntersectionTest.java
+++ src/test/java/usantatecla/IntersectionTest.java
@@ -1,5 +1,6 @@
 package usantatecla;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import org.junit.jupiter.api.Test;
 
 public class IntersectionTest {
@@ -9,5 +10,12 @@ public class IntersectionTest {
         Interval intervalA = new IntervalBuilder().closed(0).closed(10).build();
         Interval intervalB = new IntervalBuilder().closed(-5).closed(0).build();
         assertTrue(new Intersection(intervalA, intervalB).exist());
+    }
+
+    @Test
+    public void givenTwoIntervalsClosedWhenNonePointOfIntervalBInsideInIntervalAthenTrue(){
+        Interval intervalA = new IntervalBuilder().closed(0).closed(10).build();
+        Interval intervalB = new IntervalBuilder().closed(-5).closed(-1).build();
+        assertFalse(new Intersection(intervalA, intervalB).exist());
     }
 }
